cfde4cdc96ce92331b267518d6310ccd4f9a8c9d
Miguel Romero <miguel.angel.romero.gtz@gmail.com>
2021-06-01 17:04:05 -0500

    add module

--- server-two.js
+++ server-two.js
@@ -7,30 +7,32 @@ require("./db").knex;
 const Customer = require("./model/customer");
 
 function sendEmail(data) {
-var nodemailer = require('nodemailer');
-const transport = nodemailer.createTransport({
-     service: "gmail",
-     auth: {
-          user: "threebonddev@gmail.com", //your gmail account you used to set the project up in google cloud console"
-          pass: "threebonddev!1219"
-     }});
+  var nodemailer = require('nodemailer');
+  const transport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "threebonddev@gmail.com", //your gmail account you used to set the project up in google cloud console"
+      pass: "threebonddev!1219"
+    }
+  });
 
-    const aName = data.name || "NE";
-    const lastName = data.lastName || "NE";
-    const phoneNumber = data.phoneNumber || "NE";
-    const email = data.email || "NE";
-    const company = data.company || "NE";
-    const aState = data.state || "NE";
-    const sector = data.sector || "NE";
-    const otherSector = data.otherSector || "NE";
-    const novedades = data.novedades || "NO";
-    const aDescription = data.description || "NE";
+  const aName = data.name || "NE";
+  const lastName = data.lastName || "NE";
+  const phoneNumber = data.phoneNumber || "NE";
+  const email = data.email || "NE";
+  const company = data.company || "NE";
+  const aState = data.state || "NE";
+  const sector = data.sector || "NE";
+  const otherSector = data.otherSector || "NE";
+  const novedades = data.novedades || "NO";
+  const aDescription = data.description || "NE";
 
-const mailOptions = {
-  from: 'threebonddev@gmail.com', // sender address
-  to: ['lgarcia@threebond.com', 'ynishioka@threebond.com', 'omartinez@threebond.com', 'orodriguez@threebond.com'], // list of receivers
-  subject: 'Nueva solicitud de cliente', // Subject line
-  html: `<div>
+  const mailOptions = {
+    from: 'threebonddev@gmail.com', // sender address
+    to: ['miguel.angel.romero.gtz@gmail.com'], // list of receivers
+    // to: ['lgarcia@threebond.com', 'ynishioka@threebond.com', 'omartinez@threebond.com', 'orodriguez@threebond.com'], // list of receivers
+    subject: 'Nueva solicitud de cliente', // Subject line
+    html: `<div>
 	<p>Una nueva solicitud de cliente ha sido registrada</p>
 	<p>Nombre: ${aName} ${lastName}</p>
 	<p>Numero telefonico: ${phoneNumber}</p>
@@ -41,15 +43,15 @@ const mailOptions = {
 	<p>Descripción: ${aDescription}</p>
 	<p>Desea recibir novedades: ${novedades}</p>
 	</div>` // plain text body
-};
+  };
 
-transport.sendMail(mailOptions, function(error, info) {
-  if (error){
-    console.log(error);
-  } else {
-    console.log("Email sent");
-  }
-})
+  transport.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent");
+    }
+  })
 }
 
 server.use(cors());
@@ -58,8 +60,9 @@ server.set('view engine', 'ejs');
 server.use(bodyParser.urlencoded({ extended: false }))
 server.use(bodyParser.json())
 
-server.post('/api/cotiza', (req ,res) => {
+server.post('/api/cotiza', (req, res) => {
   let body = req.body;
+  console.log(body)
   let customer = new Customer({
     aName: body.name || "",
     lastName: body.lastName || "",
@@ -117,21 +120,17 @@ server.get('/', (req, res) => {
     .catch((error) => {
       res.write(JSON.stringify({ message: error }));
       console.log('something');
-	    res.end();
+      res.end();
     });
 });
 
 function createXLS(json) {
   return json.reduce((csv, row) => {
-    return `${csv}${row.aName || ""},${row.lastName || ""},${
-      row.phoneNumber || ""
-    },${row.email || ""},${row.company || ""},${row.aState || ""},${
-      row.sector || ""
-    },${
-      row.otherSector || ""
-    },${
-      row.novedades || ""
-    },${row.aDescription || ""}\n`;
+    return `${csv}${row.aName || ""},${row.lastName || ""},${row.phoneNumber || ""
+      },${row.email || ""},${row.company || ""},${row.aState || ""},${row.sector || ""
+      },${row.otherSector || ""
+      },${row.novedades || ""
+      },${row.aDescription || ""}\n`;
   }, "Nombre,Apellidos,NumeroTelefonico,Email,Compania,Estado,Sector,OtroSector,RecibirNovedades,Descripcion\n");
 }
 
